1cc36bf0fddfc4b8f7b3dbd3d913b0d6c9ea6b1d
TheMigo9 <162376189+TheMigo9@users.noreply.github.com>
2024-03-06 13:04:23 +0200

    Add files via upload

---
+++ assets/preload/stages/stage.lua
@@ -0,0 +1,10 @@
+function onCreate()
+	makeLuaSprite('stageback','W1/stageback',-650,-150)
+	addLuaSprite('stageback',false)
+	scaleObject('stageback', 4,4);
+	
+	makeLuaSprite('stagefront','W1/stagefront',-550,700)
+	addLuaSprite('stagefront',false)
+	scaleObject('stagefront', 4,4);
+	
+end
